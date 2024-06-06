9705ffdf4f712a5e6f13865ad240f12d059210f3
Yasunori-aloha <gyarubaku@yahoo.ne.jp>
2024-06-06 03:00:39 +0000

    refactor: テストコードを簡略化。

--- src/app/message/message.component.spec.ts
+++ src/app/message/message.component.spec.ts
@@ -2,41 +2,53 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessageComponent } from './message.component';
 
+import { fireEvent, render, screen } from '@testing-library/angular';
+
 describe('MessageComponent', () => {
-  let component: MessageComponent;
-  let fixture: ComponentFixture<MessageComponent>;
+  // let component: MessageComponent;
+  // let fixture: ComponentFixture<MessageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MessageComponent],
-    }).compileComponents();
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     imports: [MessageComponent],
+  //   }).compileComponents();
 
-    fixture = TestBed.createComponent(MessageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  //   fixture = TestBed.createComponent(MessageComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('.message should be "Hello"', () => {
-    // Assert
-    expect(component.message).toBe('Hello');
-  });
+  // it('.message should be "Hello"', () => {
+  //   // Assert
+  //   expect(component.message).toBe('Hello');
+  // });
 
-  it('.message should be "こんにちは" after toggleLanguage()', () => {
-    // Act
-    component.toggleLanguage();
+  // it('.message should be "こんにちは" after toggleLanguage()', () => {
+  //   // Act
+  //   component.toggleLanguage();
 
-    // Assert
-    expect(component.message).toBe('こんにちは');
-  });
+  //   // Assert
+  //   expect(component.message).toBe('こんにちは');
+  // });
 
-  it('should render "こんにちは" after toggle language button click', () => {
-    const element = fixture.nativeElement as HTMLElement;
-    const button = element.querySelector('button')!;
+  // it('should render "こんにちは" after toggle language button click', () => {
+  //   const element = fixture.nativeElement as HTMLElement;
+  //   const button = element.querySelector('button')!;
 
-    button.click();
+  //   button.click();
 
-    fixture.detectChanges();
+  //   fixture.detectChanges();
 
-    expect(element.textContent).toContain('こんにちは');
+  //   expect(element.textContent).toContain('こんにちは');
+  // });
+
+  it('should render "こんにちは" after toggle language button click', async () => {
+    // コンポーネントをレンダリング
+    await render(MessageComponent);
+
+    // 画面上のボタンをクリック
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(screen.getByText('こんにちは')).toBeDefined();
   });
 });
